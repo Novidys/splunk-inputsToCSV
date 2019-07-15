a9c66a36f36a395e25dcfd224c7ed028b35026a3
CyrilleFranchet <cf.poubelle@gmail.com>
2019-07-15 17:03:28 +0200

    Removed "

--- inputsToCSV.py
+++ inputsToCSV.py
@@ -7,7 +7,7 @@ from splunklib.client import connect
 from utils import parse
 
 
-HEADER = '"Description","Endpoint","State","Index","Sourcetype"'
+HEADER = 'Description,Endpoint,State,Index,Sourcetype'
 def main():
     opts = parse(sys.argv[1:], {}, ".splunkrc")
     service = connect(**opts.kwargs)
@@ -18,7 +18,7 @@ def main():
         if item.kind.lower() == 'rest':
             if 'sourcetype' in item.content and 'description' in item.content and 'disabled' in item.content and \
                     'endpoint' in item.content and 'index' in item.content:
-                print('"%s","%s","%s","%s","%s"' % (item.content['description'], item.content['endpoint'],
+                print('%s,%s,%s,%s,%s' % (item.content['description'], item.content['endpoint'],
                                           item.content['disabled'], item.content['index'], item.content['sourcetype']))
 
 
